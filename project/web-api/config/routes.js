87b42211c08a580d90b297ba13c1b14d3ed20829
Luis Chavarría <luiscarlosch@gmail.com>
2015-06-05 20:55:31 -0600

    Add other routes.

--- project/web-api/config/routes.js
+++ project/web-api/config/routes.js
@@ -39,6 +39,10 @@ module.exports.routes = {
   'get /items/:id/edit': 'ItemController.edit',
   'get /items/new': 'ItemController.new',
 
+  'get /orders': 'OrderController.table',
+
+  'get /categories': 'CategoryController.table',
+
   /**
    * For some reasion the blue print for delete is not working when deleting data from HTML form.
    * This temp route had to be implemented
