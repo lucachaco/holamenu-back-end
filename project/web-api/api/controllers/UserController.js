2a265bbb042f538dfffeca9d0b94cb9db44c235a
lucachaco <luiscarlosch@gmail.com>
2015-07-12 01:50:43 -0600

    Check username on login.

--- project/web-api/api/controllers/UserController.js
+++ project/web-api/api/controllers/UserController.js
@@ -11,8 +11,8 @@ module.exports = {
    * `UserController.register()`
    */
   register: function (req, res) {
-    req.body.username=req.body.email;
-    req.body.emailVerified=false;
+    req.body.username = req.body.email;
+    req.body.emailVerified = true;
     User.create(req.body).exec(function (err, result) {
       if (err) {
         return res.json(err);
@@ -53,9 +53,20 @@ module.exports = {
    * `UserController.loginCheck()`
    */
   loginCheck: function (req, res) {
-    req.session.authenticated = true;
-    console.log(req.param('username'));
-    return res.redirect('/')
+    User.findOne({username: req.param('username')}, function (err, userToReturn) {
+      if (err) {
+        console.log("err:" + err);
+        return res.redirect('/login')
+      } else {
+        console.log("user:" + userToReturn);
+        if (userToReturn && userToReturn.emailVerified) {
+          req.session.authenticated = true;
+          return res.redirect('/')
+        }else{
+          return res.redirect('/login')
+        }
+      }
+    });
   },
 
   /**
